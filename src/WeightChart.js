d05a7df4b58374f483c0e4a394c360d1f7a48b63
mdbabaylan <matmatmark@gmail.com>
2023-07-06 15:12:47 +0800

    MERN functions working

--- src/WeightChart.js
+++ src/WeightChart.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   AreaChart,
   Area,
@@ -11,53 +11,113 @@ import {
 } from "recharts";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import axios from "axios";
 
 const data = [
   {
     name: "Page A",
     uv: 4000,
-    pv: 2400,
+    // pv: 2400,
     amt: 2400
   },
   {
     name: "Page B",
-    uv: 3000,
-    pv: 1398,
+    uv: 4000,
+    // pv: 2400,
     amt: 2210
   },
   {
     name: "Page C",
-    uv: 2000,
-    pv: 9800,
+    uv: 4000,
+    // pv: 2400,
     amt: 2290
   },
   {
     name: "Page D",
     uv: 2780,
-    pv: 3908,
+    // pv: 3908,
     amt: 2000
   },
   {
     name: "Page E",
     uv: 1890,
-    pv: 4800,
+    // pv: 4800,
     amt: 2181
   },
   {
     name: "Page F",
     uv: 2390,
-    pv: 3800,
+    // pv: 3800,
     amt: 2500
   },
   {
     name: "Page G", //Page should be month X
     uv: 3490,
-    pv: 4300,
+    // pv: 4300,
     amt: 2100
   }
 ];
 
+//sample data from expressJS api get All
+
+//tweak json properties in front-end (js logic, daily,weektly,monthly, yearly weight differences)
+//a bit of modification here wouldn't hurt, and to speed up load times, making the backend lightweight and speedy as it is a free service too
+
+const apiData = 
+[
+  {
+      "_id": "64a56d3fcec2362379a55af1",
+      "date": "2023-07-05T00:00:00.000Z",
+      "user_id": 1,
+      "weight": 185.5,
+      "__v": 0
+  },
+  {
+      "_id": "64a655b8c65340a84b4573eb",
+      "date": "2023-07-06T00:00:00.000Z",
+      "user_id": 1,
+      "weight": 186.5,
+      "__v": 0
+  },
+  {
+    "_id": "64a56d3fcec2362379a55af1",
+    "date": "2023-07-07T00:00:00.000Z",
+    "user_id": 1,
+    "weight": 186,
+    "__v": 0
+  },
+  {
+      "_id": "64a655b8c65340a84b4573eb",
+      "date": "2023-07-086T00:00:00.000Z",
+      "user_id": 1,
+      "weight": 185.5,
+      "__v": 0
+  }
+];
+
+const getWeightData = () => {
+  axios.get('http://localhost:3000/api/getAll')
+    .then(function (response) {
+      // handle success
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always exe cuted
+    });
+}
+
 export default function WeightChart() {
+
+  useEffect(() => {
+    // This code will only run once, when the component mounts.
+    getWeightData();
+  }, []);
+
+
   return (
     <Container className="m-0">
         {/* <Row xs={12}> */}
